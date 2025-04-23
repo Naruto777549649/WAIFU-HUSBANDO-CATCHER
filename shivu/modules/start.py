bf1a672a64eb21108e723bd112a9cac91f2796be
Naruto777549649 <sikrar026@gmail.com>
2025-04-23 08:41:56 +0530

    Update start.py

--- shivu/modules/start.py
+++ shivu/modules/start.py
@@ -50,7 +50,7 @@ async def start(update: Update, context: CallbackContext) -> None:
             [InlineKeyboardButton("SUPPORT", url=f'https://t.me/{SUPPORT_CHAT}'),
             InlineKeyboardButton("UPDATES", url=f'https://t.me/{UPDATE_CHAT}')],
             [InlineKeyboardButton("HELP", callback_data='help')],
-            [InlineKeyboardButton("SOURCE", url=f'https://github.com/MyNameIsShekhar/WAIFU-HUSBANDO-CATCHER')]
+            [InlineKeyboardButton("SOURCE", url=f'https://github.com/Naruto777549649/WAIFU-HUSBANDO-CATCHER')]
         ]
         reply_markup = InlineKeyboardMarkup(keyboard)
         photo_url = random.choice(PHOTO_URL)
